fade3d61c104b78f0d1bc8348ab2124400c3fa41
Andrea Mendez <44706114+andreasroses@users.noreply.github.com>
2022-11-10 19:25:08 -0600

    added test code

---
+++ 10-31.java
@@ -0,0 +1,11 @@
+import java.util.*
+
+public class Main{
+	public static void Main(String[] args){
+		//important to write generic methods, so code is cleaner and method is very reusable
+		//also generic classes to hold generic methods, im assuming
+		//ArrayList<E> the <> makes what it accepts as an input super generic, so it can be a string or an int or anything
+		//for pac man game, class Ghost, isBlinking() takes in xy coords
+	}
+	
+}
